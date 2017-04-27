02a3481249bf337cb634de15dfd00ec6276157bb
ailurophile <lisalitchfield@aim.com>
2017-04-27 14:51:02 -0700

    Initial Commit

---
+++ SanJoseParkingGarages/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SanJoseParkingGarages
+//
+//  Created by Lisa Litchfield on 4/27/17.
+//  Copyright © 2017 Lisa Litchfield. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

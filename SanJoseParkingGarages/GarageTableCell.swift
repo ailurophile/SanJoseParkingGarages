41d289f11ee51c2b790acfe5bd2138f1fb74055d
ailurophile <lisalitchfield@aim.com>
2017-04-28 17:18:19 -0700

    feat: Create custom tableView cell

---
+++ SanJoseParkingGarages/GarageTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  GarageTableCell.swift
+//  SanJoseParkingGarages
+//
+//  Created by Lisa Litchfield on 4/28/17.
+//  Copyright © 2017 Lisa Litchfield. All rights reserved.
+//
+
+import UIKit
+
+class GarageTableCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var spacesLabel: UILabel!
+    @IBOutlet weak var capacityLabel: UILabel!
+}
--- SanJoseParkingGarages/GarageViewController.swift
+++ SanJoseParkingGarages/GarageViewController.swift
@@ -7,19 +7,46 @@
 //
 
 import UIKit
+import MapKit
 
-class GarageViewController: UIViewController {
 
+
+class GarageViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
+    let garages = ["garage 1", "garage 2", "garage 3"]
+    let spaces = [25,35,45]
+    let capacities = [30,100,150]
+    
+
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+// MARK: TableView Delegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        return
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GarageTableCell", for: indexPath) as! GarageTableCell
+        cell.nameLabel?.text = garages[indexPath.row]
+        cell.spacesLabel?.text = "\(spaces[indexPath.row])"
+        cell.capacityLabel?.text = "\(capacities[indexPath.row])"
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return garages.count
+    }
+    
 
 
 }
+
+
 
